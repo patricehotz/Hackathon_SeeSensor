c483ad9c6064d67e73097b1cf2737754c2326d66
SilvanMüller <silvanmueller2004@gmx.ch>
2022-10-28 18:34:56 +0200

    add SeaSensor.Repsonse Interface, Response and Class

---
+++ SeeSensor/SeaSensor.Repositories/ISeaSensorRepositories.cs
@@ -0,0 +1,16 @@
+﻿using SeaSensor.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaSensor.Repositories
+{
+    public interface ICarparkRepository
+    {
+        public SeaSensorRepoResponse addData(RepoSeaSensor repoSeaSensor);
+        public RepoSeaSensor addStatus();
+
+    }
+}
---
+++ SeeSensor/SeaSensor.Repositories/Models/RepoSeaSensor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaSensor.Repositories.Models
+{
+    public class RepoSeaSensor
+    {
+        public string Id { get; set; }
+        public string MacAdress { get; set; }
+        public int Light { get; set; }
+        public double AirTemperature { get; set; }
+        public double AirHumidity { get; set; }
+        public double AirPressure { get; set; }
+        public double WaterTemperature { get; set; }
+        public double CoordinatesY { get; set; }
+        public double CoordinatesX { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
