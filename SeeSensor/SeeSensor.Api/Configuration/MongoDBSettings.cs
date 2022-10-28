9351eec273ff5cbd6a8a75c793665e49bb98fb24
Patrice Hotz <patrice.hotz@leuchterag.ch>
2022-10-28 21:28:12 +0200

    Implementet startup and config

---
+++ SeeSensor/SeeSensor.Api/Configuration/MongoDBSettings.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace SeeSensor.Api.Configuration
+{
+    public class MongoDBSettings
+    {
+
+            [JsonProperty(PropertyName = "type")]
+            public string Type { get; set; }
+
+            [JsonProperty(PropertyName = "connectionstring")]
+            public string Connectionstring { get; set; }
+
+            [JsonProperty(PropertyName = "databasename")]
+            public string Databasename { get; set; }
+    }
+}
--- SeeSensor/SeeSensor.Api/Program.cs
+++ SeeSensor/SeeSensor.Api/Program.cs
@@ -1,3 +1,8 @@
+using SeeSensor.Api.Configuration;
+using SeaSensor.Repositories;
+using MongoDB.Driver;
+using SeeSensor.Repositories.MongoDB;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,9 +12,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var mongoDbSettings = new MongoDBSettings();
+var configDir = Environment.GetEnvironmentVariable("CONFIG_DIR");
+
+builder.Configuration.AddJsonFile(Environment.CurrentDirectory + configDir);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddMvcCore();
+
+builder.Configuration.GetSection("Persistence").Bind(mongoDbSettings);
+
+if (mongoDbSettings.Type == "mongodb")
+{
+    builder.Services.AddSingleton<ISeaSensorRepository, MongoSeeSensorRepository>();
+    builder.Services.AddSingleton<IMongoDatabase>(x => new MongoClient(mongoDbSettings.Connectionstring).GetDatabase(mongoDbSettings.Databasename));
+}
 
 var app = builder.Build();
 
