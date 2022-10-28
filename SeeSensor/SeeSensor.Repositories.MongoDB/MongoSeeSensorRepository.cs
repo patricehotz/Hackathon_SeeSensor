e7bb0d86e29584a2d7cc9a08da8a864b562790a1
SillyDrag0n <silvanmueller2004@gmx.ch>
2022-10-28 21:05:10 +0200

    add MongoSeeSensorRepository

--- SeeSensor/SeeSensor.Repositories.MongoDB/MongoSeeSensorRepository.cs
+++ SeeSensor/SeeSensor.Repositories.MongoDB/MongoSeeSensorRepository.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SeaSensor.Repositories;
+using SeaSensor.Repositories.Models;
 
 namespace SeeSensor.Repositories.MongoDB
 {
-    public class MongoSeeSensorRepository
+    public class MongoSeeSensorRepository : ISeaSensorRepository
     {
+        public SeaSensorRepoResponse addData(RepoSeaSensorData repoSeaSensor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RepoSeaSensorData addStatus(RepoSeaSensorStatus repoSeaSensor)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
