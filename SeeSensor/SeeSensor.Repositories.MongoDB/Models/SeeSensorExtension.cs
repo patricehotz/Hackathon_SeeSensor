5d00aedaacdf8f7f2614193cd4ad5feffb4fe045
SillyDrag0n <silvanmueller2004@gmx.ch>
2022-10-28 20:46:27 +0200

    add SeeSensor.Repositories.MongoDB Interface and other files

---
+++ SeeSensor/SeeSensor.Repositories.MongoDB/Models/SeeSensorExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeaSensor.Repositories.Models;
+
+namespace SeeSensor.Repositories.MongoDB.Models
+{
+    public static class ParkingSpaceExtensions
+    {
+        public static RepoSeaSensor ToRepoCarSpace(this SeeSensorMongoPersistenceModel seeSensorPersistence)
+        {
+            var seaSensor = new RepoSeaSensor()
+            {
+                Id = seeSensorPersistence.Id,
+                MacAddress = seeSensorPersistence.MacAddress,
+            };
+
+            return seaSensor;
+        }
+    }
+}
---
+++ SeeSensor/SeeSensor.Repositories.MongoDB/Models/SeeSensorMongoPersistenceModel.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeSensor.Repositories.MongoDB.Models
+{
+    public class SeeSensorMongoPersistenceModel
+    {
+        [BsonId]
+        public string Id { get; set; }
+
+        [BsonElement("macaddress")]
+        public string MacAddress { get; set; }
+
+        [BsonElement("story")]
+        public string Story { get; set; } = string.Empty;
+
+        [BsonElement("light")]
+        public string Light { get; set; } = string.Empty;
+
+        [BsonElement("airtemperature")]
+        public double AirTemperature { get; set; }
+
+        [BsonElement("airhumidity")]
+        public double AirHumidity { get; set; }
+
+        [BsonElement("airpressure")]
+        public double AirPressure { get; set; }
+
+        [BsonElement("watertemperature")]
+        public double WaterTemperature { get; set; }
+
+        [BsonElement("coordinatesY")]
+        public double CoordinatesY { get; set; }
+
+        [BsonElement("coordinatesX")]
+        public double CoordinatesX { get; set; }
+
+        [BsonElement("timestamp")]
+        public double Timestamp { get; set; }
+    }
+}
